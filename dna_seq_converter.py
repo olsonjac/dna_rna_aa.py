f36ec13d937171f96c55d7a0e5d4004c56fcfc01
Jacob Olson <50496441+olsonjac@users.noreply.github.com>
2022-01-24 09:59:38 -0500

    Update dna_seq_converter.py

--- dna_seq_converter.py
+++ dna_seq_converter.py
@@ -27,3 +27,48 @@ for i in dna:
 
 # Print the RNA string
 print( "RNA: ", rna)
+
+# mRNA Codon dictionary that will be used to translate the RNA sequence into the amino acid sequence. 
+rna_codon_dict = {"AUG" : "M", "CUU" : "L", "AUU" : "I", "GUU" : "V",
+           "UUC" : "F", "CUC" : "L", "AUC" : "I", "GUC" : "V",
+           "UUA" : "L", "CUA" : "L", "AUA" : "I", "GUA" : "V",
+           "UUG" : "L", "CUG" : "L", "UUU" : "F", "GUG" : "V",
+           "UCU" : "S", "CCU" : "P", "ACU" : "T", "GCU" : "A",
+           "UCC" : "S", "CCC" : "P", "ACC" : "T", "GCC" : "A",
+           "UCA" : "S", "CCA" : "P", "ACA" : "T", "GCA" : "A",
+           "UCG" : "S", "CCG" : "P", "ACG" : "T", "GCG" : "A",
+           "UAU" : "Y", "CAU" : "H", "AAU" : "N", "GAU" : "D",
+           "UAC" : "Y", "CAC" : "H", "AAC" : "N", "GAC" : "D",
+           "UAA" : "STOP", "CAA" : "Q", "AAA" : "K", "GAA" : "E",
+           "UAG" : "STOP", "CAG" : "Q", "AAG" : "K", "GAG" : "E",
+           "UGU" : "C", "CGU" : "R", "AGU" : "S", "GGU" : "G",
+           "UGC" : "C", "CGC" : "R", "AGC" : "S", "GGC" : "G",
+           "UGA" : "STOP", "CGA" : "R", "AGA" : "R", "GGA" : "G",
+           "UGG" : "W", "CGG" : "R", "AGG" : "R", "GGG" : "G" 
+           }
+
+
+
+
+num_codons = int(len(rna)/(3))
+limit = num_codons
+print("# of RNA codons: ", num_codons)
+
+#initializes the amino acid list
+aa_seq = []
+rna_seq = [(rna[i:i+3]) for i in range(0,len(rna), 3)]
+print("codon list:                ", rna_seq)
+
+#this block iterates through the rna codon list and the codon dictionary to
+#check for the presence of a known codon and then prints the amino acid to 
+#a new list called aa_seq
+count = 0
+while True:
+  for index in rna_seq:
+    for key in rna_codon_dict:
+      if key in rna_seq:
+        aa_seq.append(rna_codon_dict[key])
+        count = count+ 1
+        if count == limit:
+          print("the amino acid sequence is:" ,aa_seq)
+          break
